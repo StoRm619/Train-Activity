77219a093d5ab72314a1488c378260b861f3b939
Matthews A <matthewsabebe23@gmail.com>
2019-09-13 10:43:12 -0700

    check time format if statement complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,70 +13,63 @@
   // Create a variable to reference the database.
   var database = firebase.database();
   var name = "";
-  var role = "";
+  var des = "";
   var date = "";
   var rate = "";
-  var monthWork = "";
-  var totalBill = "";​
+  var nextTrain = "";
+
   $("#submitButton").on("click", function() {
       name = $("#inputName").val().trim();
-      role = $("#inputrole").val().trim();
+      des = $("#inputrole").val().trim();
       date = $("#inputDate").val().trim();
       rate = parseInt($("#inputRate").val().trim());
-      console.log(name, role, date, rate);
-      if (name != "" && role != "" && /^\d{2}\/\d{2}\/\d{4}$/.test(date) && !Number.isNaN(rate)) {
-          monthWork = Math.floor(moment(new Date()).diff(moment(date), 'months', true));
-          console.log(monthWork);
-          totalBill = monthWork * rate;​​
+      console.log(name, des, date, rate);
+      if (name != "" && des != "" && moment(date, "HH:mm", true).isValid() && !Number.isNaN(rate)) {
+          nextTrain = moment(date).add(rate, 'm');
+          console.log(nextTrain);
+
           database.ref().push({
               name,
-              role,
+              des,
               date,
               rate,
-              monthWork,
-              totalBill
+              nextTrain,
+
           });
       } else {
           alert("Data input incorrect");
       }
-  });​
+  });
   database.ref().on(
-          "child_added",
-          function(snapshot) {
-              console.log(snapshot.val())​
-              var newDiv = $("<div>")
-              newDiv.addClass("row")
-              newDiv.addClass("trainInfoRow")
-              var nameDiv = $("<div>")
-              nameDiv.text(snapshot.val().name);
-              nameDiv.addClass("col-md-2")
-              newDiv.append(nameDiv)
-              var roleDiv = $("<div>")
-              roleDiv.text(snapshot.val().role);
-              roleDiv.addClass("col-md-2")
-              newDiv.append(roleDiv)
-              var dateDiv = $("<div>")
-              dateDiv.text(snapshot.val().date);
-              dateDiv.addClass("col-md-2")
-              newDiv.append(dateDiv)
-              var monthDiv = $("<div>")
-              monthDiv.text(snapshot.val().monthWork)
-              monthDiv.addClass("col-md-2")
-              newDiv.append(monthDiv)
-              var rateDiv = $("<div>")
-              rateDiv.text(snapshot.val().rate);
-              rateDiv.addClass("col-md-2")
-              newDiv.append(rateDiv)
-              var totalDiv = $("<div>")
-              totalDiv.text(snapshot.val().totalBill);
-              totalDiv.addClass("col-md-2")
-              newDiv.append(totalDiv)
-              $("#trainDisplay").append(newDiv)
-          })
-      // var q = new Date();
-      // var m = q.getMonth();
-      // var d = q.getDay();
-      // var y = q.getFullYear();
-      ​
-      // var date = new Date(y, m, d);
-      // console.log(date);
+      "child_added",
+      function(snapshot) {
+          //console.log(snapshot.val())​;
+          var newDiv = $("<div>")
+          newDiv.addClass("row")
+          newDiv.addClass("trainInfoRow")
+          var nameDiv = $("<div>")
+          nameDiv.text(snapshot.val().name);
+          nameDiv.addClass("col-md-2")
+          newDiv.append(nameDiv)
+          var roleDiv = $("<div>")
+          roleDiv.text(snapshot.val().des);
+          roleDiv.addClass("col-md-2")
+          newDiv.append(roleDiv)
+          var dateDiv = $("<div>")
+          dateDiv.text(snapshot.val().date);
+          dateDiv.addClass("col-md-2")
+          newDiv.append(dateDiv)
+          var monthDiv = $("<div>")
+          monthDiv.text(snapshot.val().nextTrain)
+          monthDiv.addClass("col-md-2")
+          newDiv.append(monthDiv)
+          var rateDiv = $("<div>")
+          rateDiv.text(snapshot.val().rate);
+          rateDiv.addClass("col-md-2")
+          newDiv.append(rateDiv)
+              //   var totalDiv = $("<div>")
+              //   totalDiv.text(snapshot.val().totalBill);
+              //   totalDiv.addClass("col-md-2")
+              //   newDiv.append(totalDiv)
+          $("#trainDisplay").append(newDiv)
+      })
